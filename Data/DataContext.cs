b616ed5e05c21ba3ae95f08f538887a9e8cc6179
Pablo Company <p.c.ramirez5150@gmail.com>
2018-10-31 15:41:00 +0100

    Primer commit

---
+++ Data/DataContext.cs
@@ -0,0 +1,14 @@
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        // necesitamos crear una instancia de DB context options para funcionar
+        public DataContext(DbContextOptions<DataContext> options) : base (options) { }
+        // esto es para decirle a la base de datos el modelo que vamos a guardar
+        // el nombre que le pongamos va a ser el nombre con el que se cree la tabla sql
+        public DbSet<Value> Values {get; set;}
+    }
+}
